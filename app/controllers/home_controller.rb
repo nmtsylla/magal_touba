788608fb923347138403141d117753a94ea116b8
nmtsylla <niamatosylla@gmail.com>
2015-07-26 22:38:30 +0000

    import articles fil d'actu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 # encoding: utf-8
 class HomeController < ApplicationController
   def index
+    @mouride_semaine = Article.where(:rubrique_id => 6).last
+    @info_continu = Article.where(:rubrique_id => 7).limit(10)
   end
 end
