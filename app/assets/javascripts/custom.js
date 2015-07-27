ff23f4e871fd023419364baec47dd1e0d4d3fdd2
nmtsylla <niamatosylla@gmail.com>
2015-07-27 17:16:05 +0000

    update main slider

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,7 +1,35 @@
 /**
  * Created by nmtsylla on 7/25/15.
  */
+$(function(){
+    $('.newsticker').newsTicker({
+        row_height: 65,
+        max_rows: 5,
+        speed: 600,
+        direction: 'up',
+        duration: 4000,
+        autostart: 1,
+        pauseOnHover: 0
+    });
 
-$(document).ready(function() {
-    $(".owl-carousel").owlCarousel();
+    $('#news-demo').slippry({
+        // general elements & wrapper
+        slippryWrapper: '<div class="sy-box news-slider" />', // wrapper to wrap everything, including pager
+        elements: 'article', // elments cointaining slide content
+
+        // options
+        adaptiveHeight: false, // height of the sliders adapts to current
+        captions: false,
+
+        // pager
+        pagerClass: 'news-pager',
+
+        // transitions
+        transition: 'horizontal', // fade, horizontal, kenburns, false
+        speed: 1200,
+        pause: 8000,
+
+        // slideshow
+        autoDirection: 'prev'
+    });
 });
