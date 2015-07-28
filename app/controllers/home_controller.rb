e495f933b97e7638ca83b160a193731a8f1b5fab
nmtsylla <niamatosylla@gmail.com>
2015-07-28 09:44:29 +0000

    slider partial fontawesome cdn link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
   def index
     @mouride_semaine = Article.where(:rubrique_id => 6).last
     @info_continu = Article.where(:rubrique_id => 7).limit(10)
+    @a_la_une = Article.where(:rubrique_id => 1).limit(5)
   end
 end
