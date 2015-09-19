55094323638f74a1846fcd8d56c1293e2908ef48
nmtsylla <niamatosylla@gmail.com>
2015-09-19 18:17:02 +0000

    update home controller model!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,14 @@
 class HomeController < ApplicationController
   def index
     @mouride_semaine = Article.where(:rubrique_id => 6).last
+    @depeche = Article.where(:rubrique_id => 2).limit(10)
     @info_continu = Article.where(:rubrique_id => 7).limit(10)
+    @contrib = Article.where(:rubrique_id => 10).order("RANDOM()").first(5)
     @dossier = Article.where(:rubrique_id => 9).order("RANDOM()").first
     @a_la_une = Article.where(:rubrique_id => 1).limit(5)
     @pub = Article.where(:rubrique_id => 15).order("RANDOM()").first
     @sagess = Article.where(:rubrique_id => 11).order("RANDOM()").first(5)
     @a_savoir = Article.where(:rubrique_id => 5).order("RANDOM()").first(5)
-    @temoignage = Article.where(:rubrique_id => 10).order("RANDOM()").first
     @events = Evenement.where("date >= ?", Date.today ).order("date").first(5)
   end
 end
